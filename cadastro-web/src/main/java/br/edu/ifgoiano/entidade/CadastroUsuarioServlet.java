84e471f63076e85097a24a6cd99660df7f5008e6
2021108102540316 <2021108102540316@lab01-26.ifgoiano.local>
2023-03-28 10:41:55 -0300

    Adicionando botao de voltar e listar usuario no formulario

--- cadastro-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
@@ -1,6 +1,9 @@
 package br.edu.ifgoiano.entidade;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/cadastroUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
 
+	//Simulando o banco de dados 
+	private List<usuario> IstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.IstDeUsuario = new ArrayList<usuario>();
+	}
+	
 	 @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        String senha1 =  req.getParameter("senha1");
@@ -18,6 +29,10 @@ public class CadastroUsuarioServlet extends HttpServlet {
        
        //verificar se as senhas sao iguais
        if (senha1.equals(senha2)) {
+    	   usuario usu = new usuario();
+    	   usu.setNome(req.getParameter("nome"));
+    	   usu.setEmail(req.getParameter("email"));
+    	   usu.setSenha (senha1);
     	   //redrecionar o usuario para a pagina de login
     	  resp.sendRedirect("index.html"); 
        }else{
@@ -26,4 +41,18 @@ public class CadastroUsuarioServlet extends HttpServlet {
     	   
        }
 	}
+	 
+	 @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	  for (usuario usuario : IstDeUsuario) {
+		 System.out.println(usuario.getNome().concat("-").concat(usuario.getEmail()));
+	}
+	}
+	
+	 
+	 @Override
+	public void destroy() {
+		this.IstDeUsuario.clear();
+		}
+	 
 }
